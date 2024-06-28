1878dcf1e1bac33fb32af52eaa81a42ef652f2d7
Subhash Sagar Singh <sseffort@gmail.com>
2024-06-28 11:55:26 +0530

    This is first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+
+import './App.css'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import Home from './components/Home/Home'
+import { Outlet } from 'react-router-dom'
+
+function App() {
+ 
+
+  return (
+    <>
+      <Header/>
+      <Outlet/>
+      <Home/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
